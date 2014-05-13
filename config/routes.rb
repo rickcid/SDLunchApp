d81c4842acd40f1ac6ca4a380b8aa84050c25bf8
Rick Cid <rick@hispand.com>
2014-05-12 17:36:51 -0700

    Added front page styling and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 SDLunchApp::Application.routes.draw do
-  root to: 'meals#index'
+  root to: 'pages#front'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :meals, only:[:index]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
